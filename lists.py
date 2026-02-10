b3e2ca319a05d1a6ef7bc1bde662b43175125bd5
Festaa <Festa.Rexhepi@digitalschool.tech>
2026-02-10 17:50:24 +0100

    commit

---
+++ lists.py
@@ -0,0 +1,16 @@
+to_do_list = ["math homework", "reserach for biology", "clean the room", "fun"]
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+fourth_item = to_do_list[3]
+
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shopping_lists = ["apples", 'bananas', 3, 4.5]
+
+shopping_lists.append("oranges")
+print(shopping_lists)
---
+++ variables.py
@@ -0,0 +1,16 @@
+temperature = 42.3
+name = 'Festa'
+
+print(type(temperature))
+print(type(name))
+
+age = 23
+print("age is:", age)
+
+age=age+1
+print("age now is", age)
+
+first_name="Festa"
+last_name= "Rexhepi"
+full_name = first_name + " " + last_name
+print(full_name)
