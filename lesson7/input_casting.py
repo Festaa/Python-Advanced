f998f50bfe9839900bba3487818441f53bc44501
Festaa <Festa.Rexhepi@digitalschool.tech>
2026-03-10 17:37:53 +0100

    commit

---
+++ lesson7/input_casting.py
@@ -0,0 +1,9 @@
+from lesson7.implicitTypeCasting import result
+
+number1 = int(input("Enter the first number: "))
+
+number2 = int(input("Enter your second number: "))
+
+result = number1+number2
+
+print(f"The sum of {number1} and {number2} is {result}")
